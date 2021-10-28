8a4c18878068011a621a8000f3b102e218eed34e
bertmad3400 <skrivtilbertram@gmail.com>
2021-10-28 12:10:55 +0200

    Add dropdown menu for navigation

--- flaskMain.py
+++ flaskMain.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, abort, render_template, request, redirect, flash, send_file
+from flask import Flask, abort, render_template, request, redirect, flash, send_file, url_for
 
 import werkzeug
 
@@ -9,6 +9,28 @@ from ciphers import *
 app = Flask(__name__)
 app.static_folder = "./webFront/static"
 app.template_folder = "./webFront/templates"
+
+def hasNoEmptyParams(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+def getSiteMap():
+    links = []
+
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and hasNoEmptyParams(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+
+    return links
+
+@app.context_processor
+def injectSiteMap():
+    return dict(endpoints=getSiteMap())
 
 @app.errorhandler(werkzeug.exceptions.HTTPException)
 def handleHTTPErrors(e):
