ebdee430d7689d6126a516e7358c942753e5e723
bertmad3400 <skrivtilbertram@gmail.com>
2022-07-19 12:49:04 +0200

    Add functions for counting different characthers

---
+++ src/lib/statistics.js
@@ -0,0 +1,25 @@
+export const countChars = function (str, alphabet = false) {
+	return [...str].reduce((total, letter) => {
+		if (!Boolean(alphabet) || alphabet.includes(letter)) {
+			total["total"] = total["total"] + 1
+			if (!total[letter]) {
+				return {
+					...total,
+					[letter]: 1
+				}
+			} else {
+				return {
+					...total,
+					[letter]: total[letter] + 1
+				}
+			}
+		} else {
+			return total
+		}
+	}, {"total" : 0});
+};
+
+
+export const summarizeCharCount = charCounts => {
+	return Object.fromEntries(Object.entries(charCounts).filter(([char, count]) => char != "total").map(([char, count]) => [char, (count / charCounts["total"]) * 100 ]))
+}
